b50913c7ef2370a5ed3cee7226fa06a1363508d2
Elijah Chia <elijahchiakaifeng@gmail.com>
2026-05-30 15:01:41 +0700

    feat(demo): launch editor workspace on starting new campaign

--- src/App.jsx
+++ src/App.jsx
@@ -1437,7 +1437,7 @@ function CampaignAppShell({
           data={wizardData}
           onBackToEditor={() => setActiveDemoStep("editor")}
           onReset={() => {
-            window.location.href = "/local-editor";
+            setActiveDemoStep("editor");
           }}
           setView={(view) => setActiveDemoStep(view)}
           view={activeDemoStep}
